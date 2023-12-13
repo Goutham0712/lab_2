ae5bccc38aa0dba3d7e21161fa645537687236c4
Goutham0712 <patlol03@email.franklin.edu>
2023-12-13 14:44:23 +0300

    updated the get error

--- weatherapp.js
+++ weatherapp.js
@@ -1,41 +1,31 @@
-//Require express
 const express = require('express');
 const app = express();
 const port = 8002;
-
-// start server on port 8002
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-// Search stackoverflow about how to make an external API call
 const https = require('https');
 
-// Use https.get()
-https.get('https://api.weatherapi.com/v1/current.json?key=33dbd53c589146229d8194846231212&q=Paris&aqi=yes', (response) => {
-  let data = '';
+app.get('/', (req, res) => {
+  // Make the API call here
+  https.get('https://api.weatherapi.com/v1/current.json?key=33dbd53c589146229d8194846231212&q=Paris&aqi=yes', (response) => {
+    let data = '';
 
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
 
-  // Accumulate data chunks
-  response.on('data', (chunk) => {
-    data += chunk;
+    response.on('end', () => {
+      const weatherData = JSON.parse(data);
+      if (response.statusCode === 200) {
+        const temperature = weatherData.current.temp_c;
+        const description = weatherData.current.condition.text;
+        // Send the weather information as a response to the request
+        res.send(`Temperature: ${temperature}°C, Weather: ${description}`);
+      } else {
+        res.status(response.statusCode).send(`Error: ${response.statusCode}`);
+      }
+    });
   });
+});
 
-  // Process the data after receiving it
-  response.on('end', () => {
-    // Parse the result from https.get() using JSON.parse()
-    const weatherData = JSON.parse(data);
-
-    // Check if the status code is 200
-    if (response.statusCode === 200) {
-      // Get the temperature and weather description data
-      const temperature = weatherData.current.temp_c;
-      const description = weatherData.current.condition.text;
-
-      // Display on your website
-      console.log(`Temperature: ${temperature}°C, Weather: ${description}`);
-    } else {
-      console.error(`Error: ${response.statusCode}`);
-    }
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
